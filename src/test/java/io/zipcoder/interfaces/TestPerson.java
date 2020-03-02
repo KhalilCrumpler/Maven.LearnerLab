65692ea2d70ce576411016e4583b30a5a1b7f628
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-03-02 16:02:21 -0500

    added code for id test

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -9,11 +9,13 @@ public class TestPerson {
         //given
         Person khalil = new Person(1,"Khalil");
         String expected = "Khalil";
-
+        long intExpected = 1;
         //when
         String actual = khalil.getName();
-
+        long actual2 = khalil.getId();
         //then
         Assert.assertEquals(expected, actual);
+        Assert.assertEquals(intExpected, actual2);
     }
+    @Test
 }
