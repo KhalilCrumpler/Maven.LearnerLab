c5880c7479d924bc291b4b42b0d49839ae3d1666
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-03-02 19:04:04 -0500

    added more classes

---
+++ src/main/java/io/zipcoder/interfaces/Teacher.java
@@ -0,0 +1,6 @@
+package io.zipcoder.interfaces;
+
+public interface Teacher {
+    public void teach(Learner leaner, double numberOfHours);
+    public void lecture(Learner[] learners, double numberOfHours);
+}
--- src/test/java/io/zipcoder/interfaces/TestStudent.java
+++ src/test/java/io/zipcoder/interfaces/TestStudent.java
@@ -1,4 +1,33 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestStudent {
+    @Test
+    public void testImplementation(){
+        //given
+        Student khalil = new Student();
+        boolean expected = khalil instanceof Learner;
+        Assert.assertTrue(expected);
+    }
+    @Test
+    public void testInheritance(){
+        //given
+        Student khalil = new Student();
+        boolean expected = khalil instanceof Person;
+        Assert.assertTrue(expected);
+    }
+    @Test
+    public void testLearn(){
+        //given
+        Student khalil = new Student();
+        double expectedTime = 8;
+
+        //when
+        khalil.learn(8);
+        double actual = khalil.getTotalStudyTime();
+        //then
+        Assert.assertEquals(expectedTime, actual, 0.01d);
+    }
 }
