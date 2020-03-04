46b7704f34d7d2369cd4b9513cef575fdbb7c07b
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-03-04 12:26:53 -0500

    finished instructors test

--- src/test/java/io/zipcoder/interfaces/TestInstructors.java
+++ src/test/java/io/zipcoder/interfaces/TestInstructors.java
@@ -1,4 +1,22 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestInstructors {
+    @Test
+    public void testInstruct(){
+        //given
+        Instructor khalil = new Instructor();
+        Instructor khaliah = new Instructor();
+        Instructors instructors = Instructors.getInstance();
+
+        //when
+        instructors.add(khalil);
+        instructors.add(khaliah);
+
+        //then
+        Assert.assertTrue(instructors.contains(khalil));
+        Assert.assertTrue(instructors.contains(khaliah));
+    }
 }
