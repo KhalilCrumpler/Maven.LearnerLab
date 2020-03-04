731eecf9bad228e0ff10d6fe12685034cf8da6d9
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-03-04 13:34:33 -0500

    working on zipcode wilmington

--- src/main/java/io/zipcoder/interfaces/ZipCodeWilmington.java
+++ src/main/java/io/zipcoder/interfaces/ZipCodeWilmington.java
@@ -1,5 +1,10 @@
 package io.zipcoder.interfaces;
 
 public class ZipCodeWilmington {
+    private Students students = Students.getInstance();
+    private Instructors instructors = Instructors.getInstance();
 
+    public void hostLecture(Teacher teacher, double numberOfHours){
+        teacher.lecture(, numberOfHours);
+    }
 }
