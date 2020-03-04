d3b307b2bd08704d3108705652e3c423046f0061
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-03-04 14:15:23 -0500

    updated zipcodewilmington class

--- src/main/java/io/zipcoder/interfaces/Student.java
+++ src/main/java/io/zipcoder/interfaces/Student.java
@@ -3,8 +3,9 @@ package io.zipcoder.interfaces;
 public class Student extends Person implements Learner {
 
     public double totalStudyTime = 0;
+    public int count = 0;
     public Student(){
-
+        count ++;
     }
 
     public void learn(double numberOfHours) {
--- src/main/java/io/zipcoder/interfaces/ZipCodeWilmington.java
+++ src/main/java/io/zipcoder/interfaces/ZipCodeWilmington.java
@@ -1,10 +1,21 @@
 package io.zipcoder.interfaces;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class ZipCodeWilmington {
     private Students students = Students.getInstance();
     private Instructors instructors = Instructors.getInstance();
 
     public void hostLecture(Teacher teacher, double numberOfHours){
-        teacher.lecture(, numberOfHours);
+        ArrayList<Person> newList = new ArrayList();
+        for(Person a : students.personList){
+            if(a instanceof Student){
+                newList.add(a);
+            }
+        }
+        Learner[] learnerArray = new Learner[newList.size()];
+        System.arraycopy(newList, 0 , learnerArray, 0, newList.size());
+        teacher.lecture(learnerArray, numberOfHours);
     }
 }
